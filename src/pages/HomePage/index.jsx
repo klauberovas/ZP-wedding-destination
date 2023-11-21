32227c8d039b8f8ac6daed503ad8e1c61d6a2a88
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-21 09:43:31 +0100

    title + name changed

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -5,19 +5,10 @@ export const HomePage = () => {
     <div className="container">
       <header>
         <div className="logo" />
-        <h1>React webová aplikace</h1>
+        <h1>Svatba v zahraničí podle Vašich představ</h1>
       </header>
-      <main>
-        <p>
-          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí
-          {" "}
-          <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>
-          .
-        </p>
-      </main>
-      <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
-      </footer>
+      <main></main>
+      <footer></footer>
     </div>
   );
 };
