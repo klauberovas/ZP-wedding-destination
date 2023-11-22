c469de34962250c76e4b4ad9f9fe21310d0662a4
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-22 15:34:09 +0100

    banner úpravy

--- src/pages/HomePage/components/Banner/index.jsx
+++ src/pages/HomePage/components/Banner/index.jsx
@@ -3,8 +3,8 @@ import './style.css';
 export const Banner = () => {
   return (
     <section className="banner">
-      <div className="banner__title">
-        <div className="banner__logo"></div>
+      <div className="banner__title container">
+        <div className="banner__logo container"></div>
         <h1 className="title">Svatba v zahraničí podle Vašich představ</h1>
       </div>
       <div className="banner__photo"></div>
