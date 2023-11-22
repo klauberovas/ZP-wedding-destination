01a6ebc2ae4e626db57275f105dd52a7bcc0f404
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-22 11:03:03 +0100

    Menu

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import { Navbar } from '../Navbar';
+import './style.css';
+
+export const Header = () => {
+  return (
+    <header>
+      <div className="logo"></div>
+      <Navbar />
+    </header>
+  );
+};
