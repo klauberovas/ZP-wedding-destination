2297f07a3e572cc0a15f453a3c9c9680166e34cd
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-23 17:28:58 +0100

    text mobile

--- src/pages/HomePage/components/Packages/index.jsx
+++ src/pages/HomePage/components/Packages/index.jsx
@@ -6,6 +6,10 @@ export const Packages = () => {
   return (
     <section className="packages">
       <h2 className="title title--semibold title--line">Svatební balíčky</h2>
+      <p className="text--center text--mobile">
+        V naší nabídce najdete tři kategorie svatebních balíčků vztahující se na
+        všechny naše destinace.
+      </p>
       <div className="packages__container">
         {dataPackages.map((item) => (
           <Package key={item.id} item={item} />
