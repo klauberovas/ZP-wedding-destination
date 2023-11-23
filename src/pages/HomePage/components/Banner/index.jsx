97f5b7f882f3784459b9c0107a34dd90e127cb1f
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-23 14:06:50 +0100

    title class

--- src/pages/HomePage/components/Banner/index.jsx
+++ src/pages/HomePage/components/Banner/index.jsx
@@ -5,7 +5,9 @@ export const Banner = () => {
     <section className="banner">
       <div className="banner__title">
         <div className="banner__logo"></div>
-        <h1 className="title">Svatba v zahraničí podle Vašich představ</h1>
+        <h1 className="title title--regular">
+          Svatba v zahraničí podle Vašich představ
+        </h1>
       </div>
       <div className="banner__photo"></div>
     </section>
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,13 +1,15 @@
 import './style.css';
 import { Banner } from './components/Banner';
 import { Destinations } from './components/Destinations';
+import { Packages } from './components/Packages';
 
 export const HomePage = () => {
   return (
     <>
       <main>
         <Banner />
-        <Destinations/>
+        <Destinations />
+        <Packages />
       </main>
     </>
   );
