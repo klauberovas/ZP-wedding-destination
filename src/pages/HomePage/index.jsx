d186fc224e28eb59de67a7936ffb6bc597546fe9
SarkaLang <sarka.langy@gmail.com>
2023-11-23 14:14:08 +0100

    destiantion-repair

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -8,14 +8,8 @@ export const HomePage = () => {
     <>
       <main>
         <Banner />
-<<<<<<< HEAD
         </main>
         <Destinations/>
-=======
-        <Destinations />
-        <Packages />
-      </main>
->>>>>>> 97f5b7f882f3784459b9c0107a34dd90e127cb1f
     </>
   );
 };
