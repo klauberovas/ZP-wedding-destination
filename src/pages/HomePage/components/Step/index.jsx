a06ee733f6ffde9b51fb83c521063b3da588d7a7
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-24 11:17:54 +0100

    steps done

--- src/pages/HomePage/components/Step/index.jsx
+++ src/pages/HomePage/components/Step/index.jsx
@@ -1,5 +1,12 @@
 import './style.css';
 
-export const Step = () => {
-  return <div className="step"></div>;
+export const Step = ({ icon, text, alt }) => {
+  return (
+    <div className="step">
+      <div className="step__icon">
+        <img src={icon} alt={alt} />
+      </div>
+      <p className="step__text">{text}</p>
+    </div>
+  );
 };
--- src/pages/HomePage/components/Steps/index.jsx
+++ src/pages/HomePage/components/Steps/index.jsx
@@ -1,13 +1,42 @@
 import './style.css';
 import { Step } from '../Step';
 import { Button } from '../../../../components/Button';
+import search from './img/icon-search.svg';
+import calculate from './img/icon-calculate.svg';
+import telephone from './img/icon-telephone.svg';
+import yes from './img/icon-yes.svg';
+
+const data = [
+  {
+    icon: search,
+    alt: 'Icon search',
+    text: 'Vyber si svou svatební destinaci',
+  },
+  {
+    icon: calculate,
+    alt: 'Icon wedding calculate',
+    text: 'Spočítej si náklady na svatbu',
+  },
+  {
+    icon: telephone,
+    alt: 'Icon telephone',
+    text: 'Kontaktuj nás!',
+  },
+  {
+    icon: yes,
+    alt: 'Icon yes',
+    text: 'Řekni ANO!',
+  },
+];
 
 export const Steps = () => {
   return (
     <section className="steps">
       <h2 className="title title--semibold">Jak postupovat?</h2>
       <div className="steps__container">
-        <Step />
+        {data.map(({ icon, alt, text }, index) => (
+          <Step key={index} icon={icon} alt={alt} text={text} />
+        ))}
       </div>
       <Button label="Svatební kalkulačka" url="/wedding-calculate" />
     </section>
