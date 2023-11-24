7c7202c22107359b7496a5d61b7d540266704629
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-24 09:52:49 +0100

    global button

--- src/pages/HomePage/components/Step/index.jsx
+++ src/pages/HomePage/components/Step/index.jsx
@@ -1,5 +1,5 @@
 import './style.css';
 
 export const Step = () => {
-  return <div className="steps__item"></div>;
+  return <div className="step"></div>;
 };
--- src/pages/HomePage/components/Steps/index.jsx
+++ src/pages/HomePage/components/Steps/index.jsx
@@ -1,9 +1,20 @@
 import './style.css';
+import { Step } from '../Step';
+import { Link } from 'react-router-dom';
 
 export const Steps = () => {
   return (
     <section className="steps">
       <h2 className="title title--semibold">Jak postupovat?</h2>
+      <div className="steps__container">
+        <Step />
+      </div>
+      <div className="button-container">
+        <Link to="/wedding-calculate" className="button">
+          Svatební kalkulačka
+        </Link>
+      </div>
+      ;
     </section>
   );
 };
