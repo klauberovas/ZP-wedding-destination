2f8f8723d376aa7931284de27d25b81a4a2cdfc1
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-26 14:18:52 +0100

    úprava odkazu

--- src/pages/HomePage/components/Step/index.jsx
+++ src/pages/HomePage/components/Step/index.jsx
@@ -4,12 +4,21 @@ import { Link } from 'react-router-dom';
 export const Step = ({ icon, text, alt, url }) => {
   return (
     <div className="step">
-      <Link to={url === null ? '#destination' : url}>
-        <div className="step__icon">
-          <img src={icon} alt={alt} />
-        </div>
-        <p className="step__text">{text}</p>
-      </Link>
+      {url === null ? (
+        <a href="#destination">
+          <div className="step__icon">
+            <img src={icon} alt={alt} />
+          </div>
+          <p className="step__text">{text}</p>
+        </a>
+      ) : (
+        <Link to={url}>
+          <div className="step__icon">
+            <img src={icon} alt={alt} />
+          </div>
+          <p className="step__text">{text}</p>
+        </Link>
+      )}
     </div>
   );
 };
