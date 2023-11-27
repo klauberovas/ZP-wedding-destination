9277e015603a002bbd861a7e230c1a700eed8212
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-27 10:22:57 +0100

    error page úprava

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -1,3 +1,8 @@
 export const ErrorPage = () => {
-  return <h1>404 stránka nenalezna</h1>;
+  return (
+    <main className="text--center">
+      <h1>404 stránka nenalezna.</h1>
+      <h2>Na této stránce nic není 🙄</h2>
+    </main>
+  );
 };
