4d7bb94735470a3b86cb51d9142bef83d7cfeeea
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-11-29 15:37:00 +0100

    propojení radio buttonů

--- src/pages/CalculatePage/components/Form/components/RadioInput/index.jsx
+++ src/pages/CalculatePage/components/Form/components/RadioInput/index.jsx
@@ -1,9 +1,9 @@
-export const RadioInput = ({ label, image }) => {
+export const RadioInput = ({ label, image, name }) => {
   return (
     <div className="checkbox">
       <img className="checkbox__img" src={image}></img>
       <div className="checkbox__description">
-        <input className="checkbox__input" type="radio" />
+        <input className="checkbox__input" type="radio" name={name} />
         <label className="checkbox__label">{label}</label>
       </div>
     </div>
--- src/pages/CalculatePage/components/Form/components/RadioInputDown/index.jsx
+++ src/pages/CalculatePage/components/Form/components/RadioInputDown/index.jsx
@@ -1,9 +1,9 @@
 import './style.css';
 
-export const RadioInputDown = ({ label }) => {
+export const RadioInputDown = ({ label, name }) => {
   return (
     <div className="radio">
-      <input className="radio-input" type="radio" />
+      <input className="radio-input" type="radio" name={name} />
       <label htmlFor="">{label}</label>
     </div>
   );
