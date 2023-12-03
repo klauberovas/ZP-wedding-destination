603d6bd53766ab82bd5f19cfa080feb6f07db1ba
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-12-03 14:00:01 +0100

    oprava url buttonu

--- src/pages/PackagePage/Package/index.jsx
+++ src/pages/PackagePage/Package/index.jsx
@@ -1,27 +1,27 @@
-import './style.css'
+import './style.css';
 import { Button } from '../../../components/Button';
 
-export const Package = ({name, packages, photo, contains, price}) => {
-
+export const Package = ({ name, packages, photo, contains, price }) => {
   return (
     <>
-    <div className="container-destination">
-    <div className="pic-container">
-    <img className="package-pic" src={photo}></img>
-    </div>
-    <div className="package-section">
-     <h2 className="title-package">{name}</h2>
-     <div className="type-packages">{packages}</div>
-     <div className="contains" >  
-     <ul className="ul-pac">
-          {contains.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul></div>
-     <div className="package-price">{price}</div>
-     <Button label="Mám zájem" />
-    </div>
-    </div>
+      <div className="container-destination">
+        <div className="pic-container">
+          <img className="package-pic" src={photo}></img>
+        </div>
+        <div className="package-section">
+          <h2 className="title-package">{name}</h2>
+          <div className="type-packages">{packages}</div>
+          <div className="contains">
+            <ul className="ul-pac">
+              {contains.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="package-price">{price}</div>
+          <Button label="Mám zájem" url="/contact" />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
